b8b56bedd2673a0920f65b80bbfe18c920003227
Simon Lemay <simon.lemay@bell.net>
2025-08-16 11:16:55 -0400

    Delete setup-steamos.ps1

--- setup-steamos.ps1
+++
@@ -1,7 +0,0 @@
-# Copy to LOCALAPPDATA for portability
-$Target = "$env:LOCALAPPDATA\SteamOSShell"
-New-Item -Path $Target -ItemType Directory -Force | Out-Null
-Copy-Item -Path "$PSScriptRoot\*" -Destination $Target -Recurse -Force
-
-# Set custom shell
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "Shell" -Value "$Target\launch-steamos.bat"
