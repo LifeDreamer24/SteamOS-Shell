c10e34ebac02dc74783a98dbff7db88ece65a002
Simon Lemay <simon.lemay@bell.net>
2025-08-16 11:16:40 -0400

    Delete uninstall-steamos.bat

--- uninstall-steamos.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-:: Run PowerShell script from same folder as admin
-powershell -Command "&{ Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0disable-steamos.ps1"' -Verb RunAs}"
